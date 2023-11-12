818bc23ef345cc882ba76e710add38945636dbb8
lk3v1nn <kevinlmorataya@gmail.com>
2023-11-11 21:30:32 -0600

    avanznado

--- src/App.js
+++ src/App.js
@@ -10,51 +10,71 @@ import { TareaNueva } from "./components/TareaNueva";
 import { TareaLista } from "./components/TareaLista";
 
 const tareaDatos = [
-  { tarea: "Aprender React", completada: false },
-  { tarea: "Lavar carro", completada: false },
-  { tarea: "Dormir un rato", completada: false },
-  { tarea: "Preparar el almuerzo", completada: true },
+    { tarea: "Aprender React", completada: false },
+    { tarea: "Lavar carro", completada: false },
+    { tarea: "Dormir un rato", completada: false },
+    { tarea: "Preparar el almuerzo ", completada: true },
+    { tarea: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", completada: true },
 ];
 
 function App() {
-  const [tarea, setTareas] = React.useState(tareaDatos);
-  const tareasCompletadas = tarea.filter(
-    (tarea) => tarea.completada === true
-  ).length;
-  const tareasTotal = tarea.length;
+    const [tarea, setTareas] = React.useState(tareaDatos);
+    const tareasCompletadas = tarea.filter(
+        (tarea) => tarea.completada === true
+    ).length;
+    const tareasTotal = tarea.length;
 
-  const [valorBuscado, setvalorBuscado] = React.useState("");
-  const tareasBuscadas = tarea.filter(tarea => tarea.tarea.includes(valorBuscado))
-//   tareasBuscadas.map(t => console.log(t.tarea))
-  console.log(tareasBuscadas)
-  
+    const [valorBuscado, setvalorBuscado] = React.useState("");
+    const tareasBuscadas = tarea.filter((tarea) =>
+        tarea.tarea.toUpperCase().includes(valorBuscado.toUpperCase())
+    );
+    //   tareasBuscadas.map(t => console.log(t.tarea))
+    console.log(tareasBuscadas);
 
-  return (
-    <div className="app">
-      <div className="appLeft centrar">
-        <div className="cajonIzquierdo">
-          <TareaContador completadas={tareasCompletadas} total={tareasTotal} />
-          <TareaNueva />
+    const completarTarea = (text) => {
+        const nuevoTareas = [...tarea];
+        const indexTarea = nuevoTareas.findIndex((tarea) => {
+            return tarea.tarea === text;
+        });
+
+        nuevoTareas[indexTarea].completada === false
+            ? (nuevoTareas[indexTarea].completada = true)
+            : (nuevoTareas[indexTarea].completada = false);
+        setTareas(nuevoTareas);
+    };
+
+    return (
+        <div className="app">
+            <div className="appLeft centrar">
+                <div className="cajonIzquierdo">
+                    <TareaContador
+                        completadas={tareasCompletadas}
+                        total={tareasTotal}
+                    />
+                    <TareaNueva />
+                </div>
+            </div>
+
+            <div className="appRight centrar">
+                <TareaBuscador
+                    valorBuscado={valorBuscado}
+                    setvalorBuscado={setvalorBuscado}
+                />
+
+                <TareaLista>
+                    {tareasBuscadas.map((task) => (
+                        <TareaItem
+                            key={task.tarea}
+                            tareas={task}
+                            completar={() => {
+                                completarTarea(task.tarea);
+                            }}
+                        />
+                    ))}
+                </TareaLista>
+            </div>
         </div>
-      </div>
-
-      <div className="appRight centrar">
-        <TareaBuscador
-          valorBuscado={valorBuscado}
-          setvalorBuscado={setvalorBuscado}
-        />
-
-        <TareaLista>
-          {tarea.map((task) => (
-            <TareaItem
-              tareas={task}
-            //   valorBuscado={valorBuscado}
-            />
-          ))}
-        </TareaLista>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/TareaItem.js
+++ src/components/TareaItem.js
@@ -1,15 +1,21 @@
 import React from "react";
-import "../style/TareaItem.css"
+import "../style/TareaItem.css";
 
-function TareaItem({tareas}) {
-
-  return (
-      <li className={`Item ${tareas.completada && "Item--completado"}`}>
-          <span className={`icon V ${tareas.completada && "V--activo"}`}>V</span>
-          <p className={`${tareas.completada && "tarea--completada"}`}>{tareas.tarea}</p>
-          <span className="icon X X--activo">X</span>
-      </li>
-  );
+function TareaItem({ tareas, completar }) {
+    return (
+        <li className={`Item ${tareas.completada && "Item--completado"}`}>
+            <span
+                className={`icon V ${tareas.completada && "V--activo"}`}
+                onClick={()=>{completar()}}
+            >
+                V
+            </span>
+            <p className={`${tareas.completada && "tarea--completada"} text`}>
+                {tareas.tarea}
+            </p>
+            <span className="icon X X--activo">X</span>
+        </li>
+    );
 }
 
-export {TareaItem}
+export { TareaItem };
